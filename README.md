595f6d940395cfb3524cf39ac45a742a433289d5
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-02-16 20:33:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 2026
+
+A minimalistic portfolio/photography site that features a masonry grid, page transitions, and large images. It uses the Gatsby Theme [`@lekoarts/gatsby-theme-emilia`](https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emilia).
+
+## Installation
+
+To install and run the site, follow these steps:
+
+```bash
+bun install
+bun run develop
+```
+
+The site will now be running at `http://localhost:8000`!
---
+++ gatsby-config.ts
@@ -0,0 +1,57 @@
+import type { GatsbyConfig, PluginRef } from 'gatsby'
+
+const config: GatsbyConfig = {
+  // pathPrefix: `/2026`,
+  siteMetadata: {
+    siteTitle: `2026`,
+    siteTitleAlt: `2026`,
+    siteHeadline: `2026`,
+    siteUrl: `https://microlens.github.io/2026`,
+    siteDescription: 'This is 2026.',
+    siteImage: `/og-image.jpg`,
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `2026`,
+        socialMedia: [{}],
+        showThemeAuthor: false,
+        location: `Vietnam`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `2026`,
+        short_name: `2026`,
+        description: `This is 2026.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-384x384.png`,
+            sizes: `384x384`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
